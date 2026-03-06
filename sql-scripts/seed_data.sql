b9d210df548ea8efc0362ed499c5fd639fe09d08
Aksenova63 <agulkovae@gmail.com>
2026-03-07 02:01:38 +0400

    Update seed_data.sql

--- sql-scripts/seed_data.sql
+++ sql-scripts/seed_data.sql
@@ -1,1 +1,13 @@
+INSERT INTO public.clients(
+	business_name, inn, contact_phone, email) VALUES 
+	('Бест Кофе', '123456789012', '+79991234567', 'ceo@coffee.ru'),
+	('ТехноПром', '892322114522', '+79635489185', 'tech@prom.ru'),
+	('БьютиЛайн', '256987452365', '+79554785212', 'beauty@line.ru');
+
+INSERT INTO public.lawyers(
+	full_name, license_number, experience_years, spesialization, hourly_rate) VALUES
+	('Мария Непреклонная', 'LAW-555', 8, 'Трудовые споры', 4000),
+	('Иван Быстрый', 'LAW-333', 3, 'Налоги', 2500),
+	('Александр Стальной', 'LAW-777', 15, 'Арбитраж', 5000);
+
 
