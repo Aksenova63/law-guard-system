d8a0beb63b458ac1a4b94a5fa31e9b7ebb6e5b35
Aksenova63 <agulkovae@gmail.com>
2026-03-07 02:13:22 +0400

    Update seed_data.sql

--- sql-scripts/seed_data.sql
+++ sql-scripts/seed_data.sql
@@ -4,6 +4,7 @@ INSERT INTO public.clients(
 	('ТехноПром', '892322114522', '+79635489185', 'tech@prom.ru'),
 	('БьютиЛайн', '256987452365', '+79554785212', 'beauty@line.ru');
 
+
 INSERT INTO public.lawyers(
 	full_name, license_number, experience_years, spesialization, hourly_rate) VALUES
 	('Мария Непреклонная', 'LAW-555', 8, 'Трудовые споры', 4000),
@@ -11,3 +12,8 @@ INSERT INTO public.lawyers(
 	('Александр Стальной', 'LAW-777', 15, 'Арбитраж', 5000);
 
 
+INSERT INTO orders (client_id, lawyer_id, price, status) VALUES
+(1, 1, 15000, 'in_progress'),
+(2, 2, 990, 'completed'),
+(3, 1, 5000, 'in_progress'),
+(1, 3, 2500, 'completed');
